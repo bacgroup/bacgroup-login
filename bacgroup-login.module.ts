e73768613195e92d3e6a4ae2fa607a14dcf7865e
Salvatore Josue Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2018-11-29 10:41:16 -0600

    [IMP] LOGIN

---
+++ bacgroup-login.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { BacgroupLoginPage } from './bacgroup-login';
+
+@NgModule({
+  declarations: [
+    BacgroupLoginPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(BacgroupLoginPage),
+  ],
+})
+export class BacgroupLoginPageModule {}
